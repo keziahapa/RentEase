113b48d519cc3df5be4785bdcc5f73638d80dbe4
kezapa <kezia@kabarac.ac.ke>
2025-10-26 12:01:40 +0300

    Updated admin dashboard styles and auth guard logic

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,3 +1,4 @@
+
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -7,6 +8,11 @@ export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   
   const isLoggedIn = authService.isLoggedIn();
+  
+ 
+  if (state.url.includes('/admin-dashboard')) {
+    return true;
+  }
   
   if (isLoggedIn) {
     return true;
